0d48d16607e4e7adae200e2b1563b1671471179a
Alejandra <alejandra.e.bernabel@gmail.com>
2023-10-21 12:32:20 -0400

    Created db schema

--- public/js/script.js
+++ public/js/script.js
@@ -1,2 +1,5 @@
 // while loop for repeating menu?
 
+// view all departments, view all roles, view all employees, 
+//add a department, add a role, 
+//add an employee, and update an employee role
---
+++ schemas/schema.sql
@@ -0,0 +1,23 @@
+DROP DATABASE IF EXISTS employee_db;
+ADD DATABASE employee_db;
+
+CREATE TABLE departments (
+    id INT NOT NULL,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT
+);
+
+CREATE TABLE employees (
+    id INT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    -- job_title VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL
+);
