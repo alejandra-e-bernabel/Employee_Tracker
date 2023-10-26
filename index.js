59ba515c1b579adcf5acb4912293f72980671480
Alejandra <alejandra.e.bernabel@gmail.com>
2023-10-26 16:51:06 -0400

    Began working on displaying employee information

--- index.js
+++ index.js
@@ -54,43 +54,43 @@ function displayMenu() {
 
 function handleMenuChoice(choice) {
     // while (exit == 0) {
-        switch (choice) {
-            case "View all departments":
-                console.log("All depatments will be displayed");
-                displayDepartments();
-                break;
+    switch (choice) {
+        case "View all departments":
+            console.log("All depatments will be displayed");
+            displayDepartments();
+            break;
 
-            case "View all roles":
-                console.log("All roles will be displayed");
-                displayRoles();
-                displayMenu();
-                break;
+        case "View all roles":
+            console.log("All roles will be displayed");
+            displayRoles();
+            displayMenu();
+            break;
 
-            case "View all employees":
-                console.log("All employees will be displayed");
-                displayEmployees();
-                break;
+        case "View all employees":
+            console.log("All employees will be displayed");
+            displayEmployees();
+            break;
 
-            case "Add a department":
-                console.log("Department will be added");
-                break;
+        case "Add a department":
+            console.log("Department will be added");
+            break;
 
-            case "Add a role":
-                console.log("Department wil be added");
-                break;
+        case "Add a role":
+            console.log("Department wil be added");
+            break;
 
-            case "Add an employee":
-                console.log("Employee will be added");
-                break;
+        case "Add an employee":
+            console.log("Employee will be added");
+            break;
 
-            case "Update an employee":
-                console.log("Employee will be updated");
-                break;
+        case "Update an employee":
+            console.log("Employee will be updated");
+            break;
 
-            default:
-                console.log("The dafault case was accessed");
+        default:
+            console.log("The dafault case was accessed");
 
-        }
+    }
     // }
 }
 
@@ -126,15 +126,110 @@ function displayRoles() {
             console.log("\n\n");
         }
     });
-    
+
 };
 
-function displayEmployees() {
+async function displayEmployees() {
     db.query("SELECT * FROM employees", (err, results) => {
-        console.table(results, ["first-name"]);
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("\n\n");
+
+            console.log(chalk.blue.bold.underline(`ID  First Name     Last Name     Department     Salary     Manager`));
+
+            for (let i = 0; i < results.length; i++) {
+                let { id, first_name, last_name, role_id, manager_id } = results[i];
+
+                let salary;
+
+                db.query(`SELECT * FROM roles WHERE id = ${role_id}`, (err, results) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        salary = results[0].salary;
+                        let department_id = results[0].department_id;
+
+                        const department = await returnDepartment(department_id);
+                        const manager = await returnManager(manager_id);
+
+                        //formatting all results for the table
+                        if (first_name.length < 10) {
+                            first_name = first_name + ' '.repeat(10 - first_name.length);
+                        };
+
+                        if (last_name.length < 10) {
+                            last_name = last_name + ' '.repeat(10 - last_name.length);
+                        };
+
+                        if (salary.length < 10) {
+                            salary = salary + ' '.repeat(10 - salary.length);
+                        };
+
+                        if (role_id.length < 10) {
+                            role_id = role_id + ' '.repeat(10 - role_id.length);
+                        };
+
+                        // if (manager && manager.length<25) {
+                        //     manager = manager + ' '.repeat(25-manager.length);
+                        // }                                                                      
+
+                        console.log(`${id}   ${first_name}${last_name}${department}${salary}${manager}`);
+                    }
+                })
+
+            }
+
+            console.log("\n\n");
+        }
     });
+}
+
+async function returnDepartment(department_id) {
+    try {
+        return new Promise((resolve, reject) => {
+            db.query(`SELECT * FROM departments WHERE id = ${department_id}`, (err, results) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    console.table(results);
+                    const { name } = results[0];
+
+                    resolve(`${name}`);
+                }
+            });
+        });
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function returnManager(manager_id) {
+    try {
+        return new Promise((resolve, reject) => {
+            db.query(`SELECT * FROM employees WHERE id = ${manager_id}`, (err, results) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    console.table(results);
+                    const { first_name, last_name } = results[0];
+
+                    resolve(`${first_name} ${last_name}`);
+                }
+            });
+        });
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 
 //initial menu population
-displayMenu();
+// displayMenu();
+
+async function exampleUsage() {
+    const departmentName = await returnDepartment(1);
+    console.log(departmentName); // Use the managerName string here
+}
+
+exampleUsage();
