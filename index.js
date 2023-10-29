ab573253283088847aee1e863da0b76265a60e3d
Alejandra <alejandra.e.bernabel@gmail.com>
2023-10-29 17:29:46 -0400

    Finished display employee function and display departments function

--- index.js
+++ index.js
@@ -96,7 +96,19 @@ function handleMenuChoice(choice) {
 
 function displayDepartments() {
     db.query("SELECT * FROM departments", (err, results) => {
-        console.table; (results);
+
+        console.log("\n")
+        console.log (chalk.green.bold.underline('ID   Department Name'));
+        if (err) {
+            console.log(err) 
+        } else {
+            for (let i=0; i < results.length ; i ++) {
+                let {id, name} = results[i];
+
+                console.log(`${id}    ${name}`);
+
+            }
+        }
     });
 };
 
@@ -135,61 +147,108 @@ function displayRoles() {
 //line to inner join roles and departments:
 // SELECT * FROM departments INNER JOIN roles ON departments.id = roles.department_id;
 
-async function displayEmployees() {
-    db.query("SELECT * FROM employees", (err, results) => {
+function displayEmployees() {
+    db.query("SELECT employees.id, first_name, last_name, title, name, salary, manager_id FROM departments INNER JOIN roles ON departments.id = roles.department_id INNER JOIN employees ON roles.id = employees.role_id ORDER BY employees.id", async (err, results) => {
         if (err) {
             console.log(err);
         } else {
-            console.log("\n\n");
-
-            console.log(chalk.blue.bold.underline(`ID  First Name     Last Name     Department     Salary     Manager`));
+            console.log("\n");
+            console.log(chalk.blue.bold.underline(`ID  First Name     Last Name      Title               Department     Salary     Manager`));
 
             for (let i = 0; i < results.length; i++) {
-                let { id, first_name, last_name, role_id, manager_id } = results[i];
+                let {id, first_name, last_name, title, name, salary, manager_id } = results[i];
 
-                let salary;
+                let department = name;
 
-                db.query(`SELECT * FROM roles WHERE id = ${role_id}`, (err, results) => {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        salary = results[0].salary;
-                        let department_id = results[0].department_id;
+                let managerName = await returnManager(manager_id);
 
-                        const department = await returnDepartment(department_id);
-                        const manager = await returnManager(manager_id);
+                //formatting all results for the table
+                if (first_name.length < 15) {
+                    first_name = first_name + ' '.repeat(15 - first_name.length);
+                };
 
-                        //formatting all results for the table
-                        if (first_name.length < 10) {
-                            first_name = first_name + ' '.repeat(10 - first_name.length);
-                        };
+                if (last_name.length < 15) {
+                    last_name = last_name + ' '.repeat(15 - last_name.length);
+                };
 
-                        if (last_name.length < 10) {
-                            last_name = last_name + ' '.repeat(10 - last_name.length);
-                        };
+                if (salary.length < 10) {
+                    salary = salary + ' '.repeat(10 - salary.length);
+                };
 
-                        if (salary.length < 10) {
-                            salary = salary + ' '.repeat(10 - salary.length);
-                        };
+                if (title.length < 20) {
+                    title = title + ' '.repeat(20 - title.length);
+                };
+                
+                if (department.length < 15) {
+                    department = department + ' '.repeat(15-department.length);
+                }
+                
+                if (managerName.length < 20) {
+                    managerName = managerName + ' '.repeat(20-managerName.length);
+                }
 
-                        if (role_id.length < 10) {
-                            role_id = role_id + ' '.repeat(10 - role_id.length);
-                        };
-
-                        // if (manager && manager.length<25) {
-                        //     manager = manager + ' '.repeat(25-manager.length);
-                        // }                                                                      
-
-                        console.log(`${id}   ${first_name}${last_name}${department}${salary}${manager}`);
-                    }
-                })
+                console.log(`${id}   ${first_name}${last_name}${title}${department}${salary}${managerName}`);
 
             }
-
-            console.log("\n\n");
         }
     });
-}
+};
+
+// async function displayEmployees() {
+//     db.query("SELECT * FROM employees", (err, results) => {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log("\n\n");
+
+//             console.log(chalk.blue.bold.underline(`ID  First Name     Last Name     Department     Salary     Manager`));
+
+//             for (let i = 0; i < results.length; i++) {
+//                 let { id, first_name, last_name, role_id, manager_id } = results[i];
+
+//                 let salary;
+
+//                 db.query(`SELECT * FROM roles WHERE id = ${role_id}`, (err, results) => {
+//                     if (err) {
+//                         console.log(err);
+//                     } else {
+//                         salary = results[0].salary;
+//                         let department_id = results[0].department_id;
+
+//                         const department = await returnDepartment(department_id);
+//                         const manager = await returnManager(manager_id);
+
+//                         //formatting all results for the table
+//                         if (first_name.length < 10) {
+//                             first_name = first_name + ' '.repeat(10 - first_name.length);
+//                         };
+
+//                         if (last_name.length < 10) {
+//                             last_name = last_name + ' '.repeat(10 - last_name.length);
+//                         };
+
+//                         if (salary.length < 10) {
+//                             salary = salary + ' '.repeat(10 - salary.length);
+//                         };
+
+//                         if (role_id.length < 10) {
+//                             role_id = role_id + ' '.repeat(10 - role_id.length);
+//                         };
+
+//                         // if (manager && manager.length<25) {
+//                         //     manager = manager + ' '.repeat(25-manager.length);
+//                         // }                                                                      
+
+//                         console.log(`${id}   ${first_name}${last_name}${department}${salary}${manager}`);
+//                     }
+//                 })
+
+//             }
+
+//             console.log("\n\n");
+//         }
+//     });
+// }
 
 async function returnDepartment(department_id) {
     try {
@@ -216,8 +275,11 @@ async function returnManager(manager_id) {
             db.query(`SELECT * FROM employees WHERE id = ${manager_id}`, (err, results) => {
                 if (err) {
                     reject(err);
-                } else {
-                    console.table(results);
+                } else if (manager_id == null) {
+                    // console.log("there is no manager");
+                    resolve ('None');
+                }else {
+                    // console.table(results);
                     const { first_name, last_name } = results[0];
 
                     resolve(`${first_name} ${last_name}`);
@@ -230,12 +292,12 @@ async function returnManager(manager_id) {
 }
 
 
-//initial menu population
-// displayMenu();
+// initial menu population
+displayMenu();
 
-async function exampleUsage() {
-    const departmentName = await returnDepartment(1);
-    console.log(departmentName); // Use the managerName string here
-}
+// async function exampleUsage() {
+//     const departmentName = await returnDepartment(1);
+//     console.log(departmentName); // Use the managerName string here
+// }
 
-exampleUsage();
+// exampleUsage();
